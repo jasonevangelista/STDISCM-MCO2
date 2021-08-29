09aea3c5b4f61dfdfabdad65db78220a57af0c01
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2021-08-29 18:21:30 +0800

    Use space for indentation in models

--- server/models/game.js
+++ server/models/game.js
@@ -5,33 +5,33 @@ const { Player } = require("./player");
  * A class representing a game of SushiGo
  */
 class Game{
-	static _game = null;
-	static MAX_PLAYERS = 5;
+  static _game = null;
+  static MAX_PLAYERS = 5;
   static WAITING_STATE = "WAITING"; // The game is currently waiting for other players to join or be ready
   static ONGOING_STATE = "ONGOING"; // A SushiGo game is ongoing
 
-	constructor(){
-		this._id = uuidv4();
-		this._players = [];
+  constructor(){
+    this._id = uuidv4();
+    this._players = [];
     this._state = Game.WAITING_STATE;
-	}
+  }
 
   /**
    * @returns {Game} A global game object 
    */
-	static getInstance(){
-		if(Game._game == null){
-			Game._game = new Game();
-		}
-		return Game._game;
-	}
+  static getInstance(){
+    if(Game._game == null){
+      Game._game = new Game();
+    }
+    return Game._game;
+  }
 
   /**
    * @returns {string} a unique identifier for the game
    */
-	get id(){
-		return this._id;
-	}
+  get id(){
+    return this._id;
+  }
 
   /**
    * @returns {string} The current state of the game (WAITING or ONGOING)
@@ -45,41 +45,41 @@ class Game{
    * @param {Player} player The new player object to add
    * @throws {Error} When game is already full or when a player already has the same username in the game
    */
-	addPlayer(player){
-		for(let i=0; i<this._players.length; ++i){
-			if(this._players[i].username == player.username){
-				throw new Error("Username already exists");
-			}
-		}
-		if(this._players.length < Game.MAX_PLAYERS){
-			this._players.push(player);
-		}else{
-			throw new Error("The game is already full");
-		}
-	}
+  addPlayer(player){
+    for(let i=0; i<this._players.length; ++i){
+      if(this._players[i].username == player.username){
+        throw new Error("Username already exists");
+      }
+    }
+    if(this._players.length < Game.MAX_PLAYERS){
+      this._players.push(player);
+    }else{
+      throw new Error("The game is already full");
+    }
+  }
 
   /**
    * @param {string} sessionId A player's session id
    * @returns {Player} The player with the session id or undefined if none is found
    */
-	getPlayerBySessionId(sessionId){
-		return this._players.find((player) => player.sessionId == sessionId);
-	}
+  getPlayerBySessionId(sessionId){
+    return this._players.find((player) => player.sessionId == sessionId);
+  }
 
   /**
    * Gets a list of players with their user id, username, and readyState
    * 
    * @returns {Object[]} A list of objects containing public information about each player
    */
-	getPlayerList(){
-		return this._players.map((player) => {
-			return {
-				username: player.username,
-				userId: player.userId,
+  getPlayerList(){
+    return this._players.map((player) => {
+      return {
+        username: player.username,
+        userId: player.userId,
         readyState: player.readyState
-			}
-		});
-	}
+      }
+    });
+  }
 
   /**
    * Checks if the game is ready to be started. If so, changes the state of the game object from WAITING to 
--- server/models/player.js
+++ server/models/player.js
@@ -11,44 +11,44 @@ class Player {
    * 
    * @param {string} username The unique user name of the user
    */
-	constructor(username){
-		this._sessionId = uuidv4();
-		this._userId = uuidv4();
-		this._username = username;
-		this._readyStatus = false;
-	}
+  constructor(username){
+    this._sessionId = uuidv4();
+    this._userId = uuidv4();
+    this._username = username;
+    this._readyStatus = false;
+  }
 
   /**
    * @returns {string} The unique username of the user
    */
-	get username(){
-		return this._username;
-	}
+  get username(){
+    return this._username;
+  }
 
   /**
    * @return {strings} A unique public identifier
    */
-	get userId(){
-		return this._userId;
-	}
+  get userId(){
+    return this._userId;
+  }
 
   /**
    * @returns {string} a unique private identifier
    */
-	get sessionId(){
-		return this._sessionId;
-	}
+  get sessionId(){
+    return this._sessionId;
+  }
 
   /**
    * @returns {boolean} Whether the user is ready or not
    */
-	get readyStatus(){
-		return this._readyStatus;
-	}
+  get readyStatus(){
+    return this._readyStatus;
+  }
 
-	toggleReady(){
-		this._readyStatus = !this._readyStatus;
-	}
+  toggleReady(){
+    this._readyStatus = !this._readyStatus;
+  }
 }
 
 module.exports.Player = Player;
