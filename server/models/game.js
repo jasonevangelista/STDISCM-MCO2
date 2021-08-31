5a20c69ac280f49b2aa5823dde8d66e1eb6b52df
jasonevangelista <jase_evangelista@dlsu.edu.ph>
2021-08-31 12:29:15 +0800

    add card deck generator and per-round hands generator

--- server/models/game.js
+++ server/models/game.js
@@ -14,6 +14,12 @@ class Game{
     this._id = uuidv4();
     this._players = [];
     this._state = Game.WAITING_STATE;
+    
+    this._cardDeck = [];
+
+    this._handsRound1 = [];
+    this._handsRound2 = [];
+    this._handsRound3 = [];
   }
 
   /**
@@ -114,6 +120,14 @@ class Game{
     console.log("Game starting");
     io.to(this.id).emit('startGame');
     //TODO: replace with game logic
+
+    // Generate card deck
+    this._cardDeck = this.generateCardDeck();
+
+    // Divide cards into different hands for 3 rounds
+
+    // Distribute hands for first round to players
+
     setTimeout(() => {
       this.end(io);
     }, 3000);
@@ -132,6 +146,138 @@ class Game{
   }
 
   /**
+   * Generate the card deck for the game
+   * @returns {int[]} a list of cards represented by IDs which correspond to the different card types 
+   */
+  generateCardDeck(){
+    /*
+      Sushi Card ID representation:
+      1 - sashimi
+      2 - dumplings
+      3 - eel
+      4 - tofu
+    */
+    let cardDeck = [];
+
+    // total card deck == 40
+    let numSashimi = 10;
+    let numDumplings = 10;
+    let numEel = 10;
+    let numTofu = 10;
+
+    // generate individual decks per card type
+    let deckSashimi = Array(numSashimi).fill(1);
+    let deckDumplings = Array(numDumplings).fill(2);
+    let deckEel = Array(numEel).fill(3);
+    let deckTofu = Array(numTofu).fill(4);
+
+    // concat all decks into main card deck
+    cardDeck = deckSashimi.concat(deckDumplings, deckEel, deckTofu);
+  
+    // shuffle card deck
+    cardDeck = this.shuffleCardDeck(cardDeck);
+
+    return cardDeck;
+  }
+
+  /**
+   * Shuffle card deck (based on Fisher-Yates shuffle algorithm)
+   * http://sedition.com/perl/javascript-fy.html
+   * @param {int[]} cardDeck list of cards to shuffle
+   * @returns {int[]} shuffled card deck
+   */
+  shuffleCardDeck(cardDeck){
+    var currentIndex = cardDeck.length,  randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [cardDeck[currentIndex], cardDeck[randomIndex]] = [cardDeck[randomIndex], cardDeck[currentIndex]];
+    }
+
+  return cardDeck;
+
+  }
+
+
+  /**
+   * Splits the card deck into hands for 3 rounds depending on the number of players
+   * @param {int[]} cardDeck list of cards to generate hands from
+   * @param {int} playerCount number of players in the game
+   */
+  generateHands(cardDeck, playerCount){
+    let cardsPerHand = 0;
+    let numberOfRounds = 3;
+
+    let currHand = [];
+
+    let startSlice = 0;
+    let endSlice = 0;
+
+    let round1 = [];
+    let round2 = [];
+    let round3 = [];
+
+    // set cards per hand depending on number of players
+    if (playerCount == 2){
+      cardsPerHand = 10;
+    }
+    else if (playerCount == 3){
+      cardsPerHand = 9;
+    }
+    else if (playerCount == 4){
+      cardsPerHand = 8;
+    }
+    else{
+      cardsPerHand = 7;
+    }
+
+    endSlice = cardsPerHand;
+
+    for(let i = 0; i < numberOfRounds; i++){
+      // reshuffle cards per round
+      cardDeck = this.shuffleCardDeck(cardDeck);
+      // reset slice indexes
+      startSlice = 0;
+      endSlice = cardsPerHand;
+
+      for(let j = 0; j < playerCount; j++){
+        currHand = cardDeck.slice(startSlice, endSlice);
+        // update slice indexes
+        startSlice = endSlice;
+        endSlice += cardsPerHand;
+
+        // push hand to round hands list
+        if(i == 0){
+          round1.push(currHand);
+        }
+        else if(i == 1){
+          round2.push(currHand);
+        }
+        else{
+          round3.push(currHand);
+        }
+      }
+    }
+
+    // console.log('round1:')
+    // console.log(round1);
+    // console.log('round2:')
+    // console.log(round2);
+    // console.log('round3:')
+    // console.log(round3);
+
+    this._handsRound1 = round1;
+    this._handsRound2 = round2;
+    this._handsRound3 = round3;
+  }
+
+  /**
    * Computes the scores of all the players
    */
   computeAllPoints(){
@@ -145,13 +291,6 @@ class Game{
    * Computes the score of a single player
    */
   computeSushiPoints(player){
-    /*
-      Card ID representation:
-      1 - sashimi
-      2 - dumplings
-      3 - eel
-      4 - tofu
-    */
     let countSashimi = 0;
     let countDumplings = 0;
     let countEel = 0;
